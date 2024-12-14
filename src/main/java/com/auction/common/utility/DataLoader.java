71364391d61d898347e9dbe471fbe73c93ece20f
mahmoud hamed <m7modm42@gmail.com>
2024-12-14 09:48:42 +0300

    add catigories in dataLoder class

--- src/main/java/com/auction/common/utility/DataLoader.java
+++ src/main/java/com/auction/common/utility/DataLoader.java
@@ -34,7 +34,6 @@ public class DataLoader implements CommandLineRunner {
         Category mobileCategory;
         Category electronicsCategory;
         Category booksCategory;
-
         // Check if Mobile category exists, create if not
         Optional<Category> mobileOptional = categoryRepository.findByName("Mobile");
         if (mobileOptional.isEmpty()) {
@@ -42,7 +41,7 @@ public class DataLoader implements CommandLineRunner {
             mobileCategory.setName("Mobile");
             mobileCategory.setDescription("Category for mobile phones and accessories.");
             mobileCategory.setAttributes(List.of("Brand", "Model", "Screen Size", "Battery Life"));
-            mobileCategory.setImageUrl("https://images.pexels.com/photos/3756879/pexels-photo-3756879.jpeg?auto=compress&cs=tinysrgb&w=600");
+            mobileCategory.setImageUrl("https://img.freepik.com/premium-vector/colorful-mobile-phones-geometric-shapes-seamless-pattern_1362457-5115.jpg");
             categoryRepository.save(mobileCategory);
         }
 
@@ -53,6 +52,7 @@ public class DataLoader implements CommandLineRunner {
             electronicsCategory.setName("Electronics");
             electronicsCategory.setDescription("Category for general electronic devices.");
             electronicsCategory.setAttributes(List.of("Brand", "Warranty", "Power Source", "Connectivity"));
+            electronicsCategory.setImageUrl("https://thumbs.dreamstime.com/b/seamless-consumer-gadgets-electronics-pattern-television-smartphone-headset-earphones-speakers-laptop-camera-action-repetitive-195174818.jpg");
             categoryRepository.save(electronicsCategory);
         }
 
@@ -63,8 +63,65 @@ public class DataLoader implements CommandLineRunner {
             booksCategory.setName("Books");
             booksCategory.setDescription("Category for books across various genres.");
             booksCategory.setAttributes(List.of("Author", "Genre", "Publisher", "ISBN"));
+            booksCategory.setImageUrl("https://www.shutterstock.com/image-vector/seamless-book-pattern-background-wallpaper-600w-133478084.jpg");
             categoryRepository.save(booksCategory);
         }
+
+
+        Optional<Category> carsOptional = categoryRepository.findByName("Cars");
+        if (carsOptional.isEmpty()) {
+            Category carsCategory = new Category();
+            carsCategory.setName("Cars");
+            carsCategory.setDescription("Category for cars of different makes, models, and features.");
+            carsCategory.setAttributes(List.of("Make", "Model", "Year", "Engine Type", "Fuel Type", "Transmission"));
+            carsCategory.setImageUrl("https://previews.123rf.com/images/seamartini/seamartini1811/seamartini181100239/128161859-old-car-or-vintage-retro-automobile-pattern-background-vector-seamless-design-of-auto-transport.jpg");
+            categoryRepository.save(carsCategory);
+        }
+
+        Optional<Category> fashionOptional = categoryRepository.findByName("Fashion");
+        if (fashionOptional.isEmpty()) {
+            Category fashionCategory = new Category();
+            fashionCategory.setName("Fashion");
+            fashionCategory.setDescription("Category for fashion items including clothing, accessories, and more.");
+            fashionCategory.setAttributes(List.of("Brand", "Size", "Color", "Material", "Style", "Gender"));
+            fashionCategory.setImageUrl("https://static.vecteezy.com/system/resources/previews/000/163/508/non_2x/free-clothes-pattern-vectors.jpg");
+            categoryRepository.save(fashionCategory);
+        }
+
+
+        Optional<Category> animalsOptional = categoryRepository.findByName("Animals");
+        if (animalsOptional.isEmpty()) {
+            Category animalsCategory = new Category();
+            animalsCategory.setName("Animals");
+            animalsCategory.setDescription("Category for animals including pets, wildlife, and farm animals.");
+            animalsCategory.setAttributes(List.of("Species", "Breed", "Age", "Size", "Color", "Habitat"));
+            animalsCategory.setImageUrl("https://t4.ftcdn.net/jpg/07/51/53/21/360_F_751532199_kefXjCdEWMa5QsrfdwmMZ9driif3aGTR.jpg");
+            categoryRepository.save(animalsCategory);
+        }
+
+        Optional<Category> gamesOptional = categoryRepository.findByName("Games");
+        if (gamesOptional.isEmpty()) {
+            Category gamesCategory = new Category();
+            gamesCategory.setName("Games");
+            gamesCategory.setDescription("Category for games, including video games, board games, and more.");
+            gamesCategory.setAttributes(List.of("Genre", "Platform", "Developer", "Publisher", "Release Date", "Age Rating"));
+            gamesCategory.setImageUrl("https://static.vecteezy.com/system/resources/previews/002/279/007/non_2x/video-game-doodle-seamless-pattern-vector.jpg");
+            categoryRepository.save(gamesCategory);
+        }
+
+
+        Optional<Category> otherOptional = categoryRepository.findByName("Other");
+        if (otherOptional.isEmpty()) {
+            Category otherCategory = new Category();
+            otherCategory.setName("Other");
+            otherCategory.setDescription("Category for items that do not fit into other specific categories.");
+            otherCategory.setAttributes(List.of("Type", "Description", "Additional Details"));
+            otherCategory.setImageUrl("https://thumbs.dreamstime.com/b/three-dots-icon-element-simple-icon-material-style-mobile-concept-web-apps-thin-line-icon-website-design-d-124226613.jpg");
+            categoryRepository.save(otherCategory);
+        }
+
+
+
 
     }
 
